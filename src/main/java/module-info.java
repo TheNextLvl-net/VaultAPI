437f32b1a8d29c1dd852e911377e7222142af27c
david <mail@nonswag.dev>
2025-10-17 20:54:20 +0200

    Add module-info.java with module dependencies

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+// import org.jspecify.annotations.NullMarked;
+
+// @NullMarked
+module net.thenextlvl.vault {
+    requires java.logging;
+    requires org.bukkit;
+ 
+    requires static org.jspecify;
+}
